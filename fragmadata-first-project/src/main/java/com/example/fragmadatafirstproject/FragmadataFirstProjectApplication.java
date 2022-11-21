01fd44ba0c0ba1d3f2edda5198508f0a16ce1abe
AshutoshSuryawanshi <ashutoshss@fragmadata.com>
2022-11-21 21:44:35 +0530

    First project Commit

---
+++ fragmadata-first-project/src/main/java/com/example/fragmadatafirstproject/FragmadataFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.fragmadatafirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragmadataFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FragmadataFirstProjectApplication.class, args);
+	}
+
+}
